ac7a96fabec3d4a69fa63e6803381f9788736678
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2019-03-07 11:02:38 -0500

    Fully implemented multi.c

--- multi.c
+++ multi.c
@@ -1,12 +1,43 @@
 #include "multi.h"
 
+pthread_mutex_t gate = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t direction_mutex[2] = {PTHREAD_MUTEX_INITIALIZER,
+				      PTHREAD_MUTEX_INITIALIZER};
+sem_t get_rope;
+volatile int bound[2] = {0, 0};
 
+void setup()
+{
+  pthread_mutex_init(&gate, NULL);
+  pthread_mutex_init(&direction_mutex[0], NULL);
+  pthread_mutex_init(&direction_mutex[1], NULL);
+  sem_init(&get_rope, 0, 1);
+}
 
 void start_crawl(int side)
 {
+  int index = (side + 2) % 3; /* -1 --> 1
+			       *  1 --> 0 */
+  pthread_mutex_lock(&gate);
+  pthread_mutex_lock(&direction_mutex[index]);
+  bound[index]++;
+  if (bound[index] == 1) {
+    sem_wait(&get_rope);
+  }
+  printf("Starting crawl: %d\n", side);
+  pthread_mutex_unlock(&direction_mutex[index]);
+  pthread_mutex_unlock(&gate);
 }
 
 void end_crawl(int side)
 {
+  int index = (side + 2) % 3;
+  pthread_mutex_lock(&direction_mutex[index]);
+  bound[index]--;
+  printf("Finished crawl: %d\n", side);
+  if (bound[index] == 0) {
+    sem_post(&get_rope);
+  }
+  pthread_mutex_unlock(&direction_mutex[index]);
 }
 
--- multi.h
+++ multi.h
@@ -1,6 +1,17 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+
 #ifndef MULTIHEAD
 #define MULTIHEAD
 
+extern pthread_mutex_t gate;
+extern pthread_mutex_t direction_mutex[2];
+extern sem_t get_rope;
+
+volatile int bound[2];
+
+void setup();
 void start_crawl(int side);
 void end_crawl(int side);
 
